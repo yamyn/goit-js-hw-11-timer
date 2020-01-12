98d127fe19caae269b379871354a9fde8bbee65d
Yaroslav Myn <ricky.month@gmail.com>
2020-01-12 22:39:58 +0200

    Made Task 3 from module 11

---
+++ js/timer.js
@@ -0,0 +1,71 @@
+"use strict";
+
+class CountdownTimer {
+  constructor(selector, targetDate) {
+    this.selector = selector;
+    this.targetDate = targetDate.getTime();
+  }
+
+  start() {
+    this.selectRefs();
+    this.timerId = setInterval(() => {
+      const currentDate = Date.now();
+      const time = this.targetDate - currentDate;
+      if (time < 0) {
+        clearInterval(this.timerId);
+        return console.log("Your date is ago!");
+      }
+      this.updateTimerFace(time);
+    }, 1000);
+  }
+
+  selectRefs() {
+    this.timerFace = document.querySelector(this.selector);
+    this.days = this.timerFace.querySelector(".value[data-value=days]");
+    this.hours = this.timerFace.querySelector(".value[data-value=hours]");
+    this.mins = this.timerFace.querySelector(".value[data-value=mins]");
+    this.secs = this.timerFace.querySelector(".value[data-value=secs]");
+  }
+
+  updateTimerFace(time) {
+    const daysValue = this.calculateForDays(time);
+    const hoursValue = this.calculateForHours(time);
+    const minsValue = this.calculateForMins(time);
+    const secsValue = this.calculateForSecs(time);
+
+    this.updateTextContent(this.days, daysValue);
+    this.updateTextContent(this.hours, hoursValue);
+    this.updateTextContent(this.mins, minsValue);
+    this.updateTextContent(this.secs, secsValue);
+  }
+
+  updateTextContent(elem, value) {
+    elem.textContent = `${value}`;
+  }
+
+  calculateForDays(time) {
+    return this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  }
+
+  calculateForHours(time) {
+    return this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+  }
+
+  calculateForMins(time) {
+    return this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  }
+
+  calculateForSecs(time) {
+    return this.pad(Math.floor((time % (1000 * 60)) / 1000));
+  }
+
+  pad(value) {
+    return String(value).padStart(2, 0);
+  }
+}
+
+const newTimer = new CountdownTimer("#timer-1", new Date("Jan 17, 2020"));
+
+newTimer.start();
